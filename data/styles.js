178225f0ff1e92240e4f1b2186ef2632547a56ab
Heidi El-Hosaini <heidi.elhosaini@gmail.com>
2015-04-20 15:25:57 +0100

    Added New Confirmed & Cumulative Confirmed layers

--- data/styles.js
+++ data/styles.js
@@ -54,6 +54,42 @@ var totalCasesStyle = function(feature){
         }
 };
 
+var newConfStyle = function(feature){
+        if(feature.properties.PCODEUSE in NewConfirms) {
+            if(NewConfirms[feature.properties.PCODEUSE]<1){
+                return {color: color[0],fillColor: color[0],fillOpacity:0.6,opacity:0.7,weight:2};
+            } else if(NewConfirms[feature.properties.PCODEUSE]<5){
+                return {color: color[1],fillColor: color[1],fillOpacity:0.6,opacity:0.7,weight:2};
+            } else if(NewConfirms[feature.properties.PCODEUSE]<10){
+                return {color: color[2],fillColor: color[2],fillOpacity:0.6,opacity:0.7,weight:2};
+            } else if(NewConfirms[feature.properties.PCODEUSE]<20){
+                return {color: color[3],fillColor: color[3],fillOpacity:0.6,opacity:0.7,weight:2};
+            } 
+            return {color: color[4],fillColor: color[4],fillOpacity:0.6,opacity:0.7,weight:2};
+
+        } else {
+            return {"color": "none","opacity":1};
+        }
+};
+
+var cumConfStyle = function(feature){
+        if(feature.properties.PCODEUSE in CumConfirms) {
+            if(CumConfirms[feature.properties.PCODEUSE]<1){
+                return {color: color[0],fillColor: color[0],fillOpacity:0.6,opacity:0.7,weight:2};
+            } else if(CumConfirms[feature.properties.PCODEUSE]<200){
+                return {color: color[1],fillColor: color[1],fillOpacity:0.6,opacity:0.7,weight:2};
+            } else if(CumConfirms[feature.properties.PCODEUSE]<500){
+                return {color: color[2],fillColor: color[2],fillOpacity:0.6,opacity:0.7,weight:2};
+            } else if(CumConfirms[feature.properties.PCODEUSE]<2000){
+                return {color: color[3],fillColor: color[3],fillOpacity:0.6,opacity:0.7,weight:2};
+            } 
+            return {color: color[4],fillColor: color[4],fillOpacity:0.6,opacity:0.7,weight:2};
+
+        } else {
+            return {"color": "none","opacity":1};
+        }
+};
+
 var medicalCentresStyle = function(feature){
     if(feature.properties.Status == "Open"){
         return   {radius: 5,
