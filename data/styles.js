0d3f6c8788bb6fe63d833013a1b99ac7e5ad6910
Heidi El-Hosaini <heidi.elhosaini@gmail.com>
2015-04-24 14:07:58 +0100

    Removed New Cases layer

--- data/styles.js
+++ data/styles.js
@@ -1,23 +1,5 @@
 var color = ["none","#ffe082","#ffbd13","#ff8053","#ff493d"];
 
-var newCasesStyle = function(feature){
-        if(feature.properties.PCODEUSE in newCases) {
-            if(newCases[feature.properties.PCODEUSE]<1){
-                return {color: color[0],fillColor: color[0],fillOpacity:0.6,opacity:0.7,weight:2};
-            } else if(newCases[feature.properties.PCODEUSE]<25){
-                return {color: color[1],fillColor: color[1],fillOpacity:0.6,opacity:0.7,weight:2};
-            } else if(newCases[feature.properties.PCODEUSE]<50){
-                return {color: color[2],fillColor: color[2],fillOpacity:0.6,opacity:0.7,weight:2};
-            } else if(newCases[feature.properties.PCODEUSE]<200){
-                return {color: color[3],fillColor: color[3],fillOpacity:0.6,opacity:0.7,weight:2};
-            } 
-            return {color: color[4],fillColor: color[4],fillOpacity:0.6,opacity:0.7,weight:2};
-
-        } else {
-            return {"color": "none","opacity":1};
-        }
-};
-
 var totalDeathsStyle = function(feature){
         if(feature.properties.PCODEUSE in totalDeaths) {
             if(totalDeaths[feature.properties.PCODEUSE]<1){
