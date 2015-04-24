319446291bb82521dafc3c9aedabffeee546fe72
Heidi El-Hosaini <heidi.elhosaini@gmail.com>
2015-04-24 14:07:23 +0100

    Removed New Cases layer

--- data/legends.js
+++ data/legends.js
@@ -1,12 +1,3 @@
-newCasesLegendContent = function(){
-    var labels = ["0","1-24","25-49","50-199","200+"];
-    var html = "<p>New Cases in the last 4 weeks</p>";
-    for(i=0;i<5;i++){
-        html = html +'<p><i style="background-color:' + color[i]+'"></i> '+labels[i]+'</p>';
-    }
-    return html;
-};
-
 totalCasesLegendContent = function(){
     var labels = ["0","1-99","100-299","300-799","800+"];
     var html = "<p>Total Cases</p>";
